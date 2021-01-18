abcbbe0ab0b54dfd5737ddafc9949410d0654ea6
markfour <axhfaro@gmail.com>
2021-01-19 00:24:34 +0900

    Add TagCollectionView

---
+++ TagCollectionView/TagCollectionView.swift
@@ -0,0 +1,27 @@
+//
+//  TagCollectionView.swift
+//  TagCollectionView
+//
+//  Created by kazumi hayashida on 2021/01/19.
+//
+
+import UIKit
+
+final class TagCollectionView: UICollectionView {
+  static func instance(frame: CGRect) {
+    let view = TagCollectionView(frame: frame,
+                                 collectionViewLayout: UICollectionViewLayout())
+    view.dataSource = view
+    return
+  }
+}
+
+extension TagCollectionView: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    return UICollectionViewCell()
+  }
+}
